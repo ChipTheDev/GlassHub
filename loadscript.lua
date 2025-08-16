1e815f57a802dafa829bf14c6b723104c77367db
Chip Hill <itsyodaisuke@gmail.com>
2025-08-16 09:40:10 -0500

    Update loadscript.lua

--- loadscript.lua
+++ loadscript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ChipTheDev/ImpossibleGlassBridge/7b96d502217532bd00981dac95f1b43b45f2bd2a/ImpossibleGlassBridge.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ChipTheDev/ImpossibleGlassBridge/refs/heads/main/ImpossibleGlassBridgeV2.txt"))()
