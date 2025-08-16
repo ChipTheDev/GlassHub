9f906d24b5b9eca213581320df37c05bb9e8497d
Chip Hill <itsyodaisuke@gmail.com>
2025-08-16 15:29:59 -0500

    Update load.lua

--- main/load/load.lua
+++ main/load/load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ChipTheDev/GlassHub/refs/heads/main/main/gui/GlassHubV2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ChipTheDev/GlassHub/refs/heads/main/main/gui/GlassHubV3.txt"))()
